d114d1626eff62ab7d47c66bc8fbfef7db9ab515
patrickfleith <pfleith.pro@gmail.com>
2023-06-29 22:10:44 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PDFraken',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/patrickfleith/PDFraken',
     author='Patrick Fleith',
     author_email='',
